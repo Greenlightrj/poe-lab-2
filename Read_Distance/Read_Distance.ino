f3a39f1df2751d3d07e44b52a75e2c893c2b1737
Greenlightrj <greentigerr@cox.net>
2015-09-25 11:20:24 -0400

    code reads from distance sensor and prints to serial

---
+++ Read_Distance/Read_Distance.ino
@@ -0,0 +1,18 @@
+//initialize variables up here so they can be used in everything
+int dsense;
+
+void setup() {
+  // put your setup code here, to run once:
+//initialize input pin
+pinMode(A0, INPUT);
+// begin serial so we can print values
+Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+//read from distance sensor
+dsense = analogRead(A0);
+//print value
+Serial.println(dsense);
+}
